49db57bb47eee2daa9013bbc80aff05153cc2455
DL <deleucecile@outlook.fr>
2019-10-31 11:29:33 +0100

    fin p5 avec correction

--- Exercices-Javascript-Partie-5-master/questions.js
+++ Exercices-Javascript-Partie-5-master/questions.js
@@ -13,6 +13,7 @@ var ElementReplacement = function (languages) {
   // J'accéde par la fonction à l'item Java qui se trouve à la 2éme position.Je le remplace par Javascript.
   return languages;
 }
+//  correction languages.splice(2,1,'Javascript");
 
 var AddElementToLanguagesArray = function (languages) {
   languages.push('Ruby','Python');
@@ -35,23 +36,25 @@ languages.pop();
 }
 
 var stringToArray = function (socialMediaInString) {
-socialMedia = socialMediaInString.split(',');
+ var socialMedia = socialMediaInString.split(',');
 // le socialMedia recoit le reste de la chaine donc on le met en 1er. On retourne le tableau socialMedia.
   return socialMedia;
 }
 
 var arrayToString = function (languages) {
-
-  return 'A compléter';
+ var languagesInString = languages.join(',');
+  return languagesInString;
 }
+//le join:  permet de transformer un tableau en chaine de caractère
+
 
 var arraySort = function (socialMedia) {
 
-  return 'A compléter';
+  return socialMedia.sort();
 }
 
 var arrayInvert = function (languages){
 
-
-  return 'A compléter';
+languagesInvert =languages.reverse()
+  return languages ;
 }
